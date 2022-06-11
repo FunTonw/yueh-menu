321dc7a82e0d7811977aaaffae61c9a4a33568d7
Adam <qw321888@gmail.com>
2022-06-11 16:02:38 +0800

    add: PreProducts

--- src/components/PreItem.vue
+++ src/components/PreItem.vue
@@ -2,14 +2,20 @@
   <div class="bg-white rounded-3" style="width: 90vw;">
     <div class="bg-white p-2 m-3">
       <div class="d-flex flex-column lh-1">
-        <h2>古早味飯糰</h2>
-        <div class="topping-list ">
+        <p class="fs-4 mb-2">古早味飯糰</p>
+        <div class="topping-list lh-1">
           <!-- 這邊會先有原本的配料，之後再加上點擊的配料 -->
           <span>+ 海苔絲</span>
           <span>+ 鹹蛋黃</span>
           <span>+ 蔥蛋</span>
+                    <span>+ 海苔絲</span>
+          <span>+ 鹹蛋黃</span>
+          <span>+ 蔥蛋</span>
+                    <span>+ 海苔絲</span>
+          <span>+ 鹹蛋黃</span>
+          <span>+ 蔥蛋</span>
         </div>
-        <hr class="text-success rounded-1" style="padding-top: 3px;">
+        <hr class="text-success rounded-1 m-1" style="padding-top: 3px;">
         <p class="total align-self-end fs-2">$ 120</p>
       </div>
     </div>
--- src/components/PreProducts.vue
+++ src/components/PreProducts.vue
@@ -1,17 +1,90 @@
 <template>
-  <div class="bg-white rounded-3 p-3" style="width: 90vw; height:max-content">
-    <div>
-      <div class="d-flex flex-column lh-1">
-        <p>古早味飯糰</p>
-        <div class="topping-list ">
-          <!-- 這邊會先有原本的配料，之後再加上點擊的配料 -->
-          <span>+ 海苔絲</span>
-          <span>+ 鹹蛋黃</span>
-          <span>+ 蔥蛋</span>
-        </div>
-        <hr class="text-success rounded-1" style="padding-top: 3px;">
-        <p class="total align-self-end fs-2">$ 120</p>
-      </div>
-    </div>
+  <div class="bg-white rounded-3 p-3 overflow-scroll" style="width: 90vw; height: 25vh">
+    <ul class="p-0">
+      <li>
+        <Swiper
+        :grabCursor="true"
+        :effect="'creative'"
+        :creativeEffect="{
+          prev: {
+            shadow: true,
+            origin: 'left center',
+            translate: ['-5%', 0, -200],
+            rotate: [0, 100, 0],
+          },
+          next: {
+            origin: 'right center',
+            translate: ['5%', 0, -200],
+            rotate: [0, -100, 0],
+          },
+        }"
+        :modules="modules">
+          <SwiperSlide>
+            <div class="d-flex flex-column lh-1" ref="box_1" v-on:click="reload()">
+              <p class="fs-4 mb-2"><span>1.</span>古早味飯糰</p>
+              <div class="topping-list lh-1">
+                <!-- 這邊會先有原本的配料，之後再加上點擊的配料 -->
+                <span>+ 海苔絲</span>
+                <span>+ 鹹蛋黃</span>
+                <span>+ 蔥蛋</span>
+              </div>
+              <hr class="text-success rounded-1 m-1" style="padding-top: 3px;">
+              <p class="total align-self-end fs-2">$ 120</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+              <button class="btn btn-danger w-100" v-on:click="del()"
+               :style="'height:' + boxHeight[0] + 'px' ">
+                <i class="fa-solid fa-trash-can"></i>
+              </button>
+          </SwiperSlide>
+        </Swiper>
+      </li>
+    </ul>
   </div>
+
 </template>
+
+<style>
+</style>
+
+<script>
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import 'swiper/css';
+  import "swiper/css/effect-creative";
+  import { EffectCreative } from "swiper";
+
+export default {
+    setup() {
+    return {
+      modules: [EffectCreative],
+    };
+  },
+  components :{
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      PreProducts:[],
+      boxHeight: [],
+    };
+  },
+  methods: {
+    del() {
+      console.log('del');
+    },
+    reload() {
+      console.log('reload');
+    },
+    setheight(index) {
+      for(let i = 0; i < index; i ++){
+        this.boxHeight.push(this.$refs[`box_${i+1}`].offsetHeight);
+      }
+    },
+  },
+  mounted() {
+    this.setheight(1);
+  }
+}
+</script>
